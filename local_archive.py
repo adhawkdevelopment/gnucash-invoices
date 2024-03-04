1e1c0e7c459d1401b36fd787571f8400f70d0441
Kevin Hall <visions@adhawkdevelopment.com>
2024-03-03 22:29:36 -0800

    added archive script

---
+++ local_archive.py
@@ -0,0 +1,10 @@
+import os
+
+
+GNUCASH_ARCHIVE_FILEPATH = '/var/gnucash/archive/'
+if __name__ == "__main__":
+    list = os.listdir(GNUCASH_ARCHIVE_FILEPATH)
+    list.sort()
+    if len(list) > 8:
+        os.remove(GNUCASH_ARCHIVE_FILEPATH + list[0])
+    os.system("rsync -r /var/gnucash/<YOUR_DB_NAME> /var/gnucash/archive/<YOUR_ARCHIVED_DB_NAME>")
