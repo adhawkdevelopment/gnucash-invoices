7cf51ba501db5b73f20615bcb9724af663b09a34
Kevin Hall <visions@adhawkdevelopment.com>
2024-03-04 21:45:23 -0800

    Update local_archive.py

--- local_archive.py
+++ local_archive.py
@@ -5,6 +5,6 @@ GNUCASH_ARCHIVE_FILEPATH = '/var/gnucash/archive/'
 if __name__ == "__main__":
     list = os.listdir(GNUCASH_ARCHIVE_FILEPATH)
     list.sort()
-    if len(list) > 8:
+    while len(list) > 8:
         os.remove(GNUCASH_ARCHIVE_FILEPATH + list[0])
     os.system("rsync -r /var/gnucash/<YOUR_DB_NAME> /var/gnucash/archive/<YOUR_ARCHIVED_DB_NAME>")
